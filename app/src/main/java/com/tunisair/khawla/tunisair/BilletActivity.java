9d455a8c096694a40f767a10081283a31f3cd6b3
khawla <kaoulatouati@gmail.com>
2018-04-06 15:22:57 +0200

    final

--- app/src/main/java/com/tunisair/khawla/tunisair/BilletActivity.java
+++ app/src/main/java/com/tunisair/khawla/tunisair/BilletActivity.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ProfilActivity extends AppCompatActivity
+public class BilletActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil);
+        setContentView(R.layout.activity_billet);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,7 +46,7 @@ public class ProfilActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.profil, menu);
+        getMenuInflater().inflate(R.menu.billet, menu);
         return true;
     }
 
