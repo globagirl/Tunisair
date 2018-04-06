26fcaf2dc244acd1e9418c784e90f41b03bb402a
ghada <ghadagasmi14@gmail.com>
2018-04-06 11:55:48 +0200

    test

--- app/src/main/java/com/tunisair/khawla/tunisair/InscriptionActivity.java
+++ app/src/main/java/com/tunisair/khawla/tunisair/InscriptionActivity.java
@@ -13,6 +13,8 @@ import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InscriptionActivity extends AppCompatActivity {
 
@@ -28,6 +30,32 @@ public class InscriptionActivity extends AppCompatActivity {
         EditText conf_password= (EditText) findViewById(R.id.conf_password);
         EditText num_pass= (EditText) findViewById(R.id.num_pass);
 
+        protected void onclick(View v){
+            if (nom.getText().toString().equals("")){
+
+
+            }
+            // On déclare le pattern que l’on doit vérifier
+            Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+            // On déclare un matcher, qui comparera le pattern avec la
+            // string passée en argument
+            Matcher m = p.matcher((CharSequence) nom);
+            // Si l’adresse mail saisie ne correspond au format d’une
+            // adresse mail on un affiche un message à l'utilisateur
+            if (!m.matches()) {
+                // Toast est une classe fournie par le SDK Android
+                // pour afficher les messages (indications) à l'intention de
+                // l'utilisateur. Ces messages ne possédent pas d'interaction avec l'utilisateur
+                // Le premier argument représente le contexte, puis
+                // le message et à la fin la durée d'affichage du Toast (constante
+                // LENGTH_SHORT ou LENGTH_LONG). Sans oublier d'appeler la méthode
+                //show pour afficher le Toast
+                return;
+
+
+
+        }
+    }
 
     }
 
