05939d83ab6437438fddd9359a92b9606d1dfb2e
khawla <kaoulatouati@gmail.com>
2018-05-07 15:51:51 +0200

    splash

--- app/src/main/java/com/tunisair/khawla/tunisair/Splash_Screen.java
+++ app/src/main/java/com/tunisair/khawla/tunisair/Splash_Screen.java
@@ -1,30 +1,68 @@
 package com.tunisair.khawla.tunisair;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Splash_Screen extends AppCompatActivity {
-
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        StartAnimations();
+    }
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
 
-        Thread thread=new Thread(){
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(4000);
-                    Intent ite=new Intent(getApplicationContext(),LoginActivity.class);
-                    startActivity(ite);
-                    finish();
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 6000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splash_Screen.this,
+                            LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splash_Screen.this.finish();
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    // do nothing
+                } finally {
+                    Splash_Screen.this.finish();
                 }
+
             }
         };
-        thread.start();
+        splashTread.start();
 
     }
+
+
 }
